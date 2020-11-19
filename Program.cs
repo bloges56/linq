e579e2794c7db1e3b8bfbc9996448d4db0637f19
bloges56 <brady.drew.logan@gmail.com>
2020-11-19 11:22:59 -0600

    filter multiples of 4 and 6

--- Program.cs
+++ Program.cs
@@ -12,12 +12,25 @@ namespace linq
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             IEnumerable<string> LFruits = from fruit in fruits
-                where fruit.StartsWith('L')
-                select fruit;
-            
+                                          where fruit.StartsWith('L')
+                                          select fruit;
+
             // foreach(string fruit in LFruits)
             // {
             //     Console.WriteLine(fruit);
+            // }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
+
+            // foreach (int num in fourSixMultiples)
+            // {
+            //     Console.WriteLine(num);
             // }
         }
     }
